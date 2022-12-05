c68ea10652f6eaec0357df5312bbf410fb09cd28
Matze <80168699+matze-infosec@users.noreply.github.com>
2022-12-04 20:10:19 -0800

    Adding SwiftOnSysmon
    Adding SwiftOnSysmon

---
+++ SwiftOnSysmon/Sysmon.ps1
@@ -0,0 +1,14 @@
+﻿function chooseVersion {
+cls
+echo "Choose instalation type"
+echo "1 = Default installation"
+echo "2 = Custom instalation (SwiftOnSecurity)"
+
+$version = read-host -prompt "Instalation type? (1/2)"
+
+if ($version -eq 1){C:\Sysmon\Sysmon\Sysmon.exe -accepteula -i }
+elseif ($version -eq 2){C:\Sysmon\Sysmon\Sysmon.exe -accepteula -i custom.xml}
+else {echo "incorrect option selected";cls; chooseVersion}
+}
+
+chooseVersion
